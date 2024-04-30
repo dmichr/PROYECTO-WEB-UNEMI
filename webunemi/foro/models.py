1a3c2630fb799ff5b2016f6db87c291525d06425
dmichr <dromeroo@unemi.edu.ec>
2024-04-29 23:46:54 -0500

    modelos

--- webunemi/foro/models.py
+++ webunemi/foro/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Usuario(models.Model):
     nombre = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
+    usuario = models.CharField(unique=True,max_length=15)
     contraseña = models.CharField(max_length=100)
 
 class Registro(models.Model):
